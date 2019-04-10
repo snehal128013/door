957a47d1a3fd55b856d97ccf64b3f61197aaa1c7
snehal128013 <49471381+snehal128013@users.noreply.github.com>
2019-04-10 16:32:01 +0530

    Update fact.c

--- fact.c
+++ fact.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
 int main()
 {
-  int c, n, fact = 1;
+  int c, n=4, fact = 1;
  
-  printf("Enter a number to calculate its factorial\n");
-  scanf("%d", &n);
+ 
  
   for (c = 1; c <= n; c++)
     fact = fact * c;
